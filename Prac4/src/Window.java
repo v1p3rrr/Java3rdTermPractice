7ca9b4dd38b8761214c5725e87460ea8467fb4a6
v1p3rrr <v1p3rrr@users.noreply.github.com>
2020-12-08 01:53:18 +0300

    Prac4

---
+++ Prac4/src/Window.java
@@ -0,0 +1,61 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Window extends JFrame
+{
+    JFrame window = new JFrame("Practice #4 Window");
+    Font defFont = new Font("Acrobat Bold", Font.PLAIN, 20);
+    JButton team1 = new JButton("Spacestation Gaming");
+    JButton team2 = new JButton("Team Empire");
+    private int t1 = 0;
+    private int t2 = 0;
+    JLabel result = new JLabel("Result "+t1+":"+t2);
+    JLabel lastScorer = new JLabel("Last Scorer: N/A");
+    Label winner = new Label("Winner: DRAW");
+    public Window()
+    {
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setSize(320, 280);
+        window.setLayout(new FlowLayout());
+
+        team1.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {Clicked(true);}});
+        team2.addActionListener(e -> Clicked(false)); // same <-^
+
+        window.add(team1);
+        window.add(team2);
+        window.add(result);
+        window.add(winner);
+        winner.setFont(defFont);
+        result.setFont(defFont);
+        window.add(lastScorer);
+
+
+    }
+
+    private void Clicked(boolean x)
+    {
+        if (x)
+        {
+            t1+=1;
+            result.setText("Result "+t1+":"+t2);
+            lastScorer.setText("Last Scorer: "+team1.getText());
+        }
+        else
+        {
+            t2+=1;
+            result.setText("Result "+t1+":"+t2);
+            lastScorer.setText("Last Scorer: "+team2.getText());
+        }
+        if (t1>t2)
+            winner.setText("Winner: "+team1.getText());
+        else if (t2>t1)
+            winner.setText("Winner: "+team2.getText());
+        else
+            winner.setText("Winner: DRAW");
+
+    }
+}
