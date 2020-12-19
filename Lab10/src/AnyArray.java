be92dc259b66fca690bacb73419f24ff05967b54
v1p3rrr <v1p3rrr@users.noreply.github.com>
2020-12-19 04:47:56 +0300

    Lab10 upd

--- Lab10/src/AnyArray.java
+++ Lab10/src/AnyArray.java
@@ -29,3 +29,53 @@ public class AnyArray<E>{
         return Arrays.toString(content);
     }
 }
+
+
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Collection;
+//
+//// Написать класс, который умеет хранить в себе МАССИВ любых типов данных
+//public class AnyArray<E>{
+//
+//    private Object[] content;
+//    private Class<?>[] classes;
+//
+//    public AnyArray(){
+//        content = new Object[0];
+//        classes = new Class<?>[0];
+//    }
+//
+//    public AnyArray(E[] array){
+//        content = array;
+//        classes = new Class<?>[array.length];
+//        for (int i = 0; i<array.length; i++){
+//            classes[i]=array[i].getClass();
+//        }
+//    }
+//
+//    public void add(E e){
+//        content = Arrays.copyOf(content, content.length + 1);
+//        content[content.length-1] = e;
+//        classes = Arrays.copyOf(classes, classes.length + 1);
+//        classes[classes.length-1] = e.getClass();
+//    }
+//
+//    public int size() {
+//        return content.length;
+//    }
+//
+//    // Реализовать метод, который возвращает любой элемент массива по индексу
+//    public Object get(int i){
+//        return classes[i].cast(content[i]);
+//    }
+//
+//    public void replace(E[] e){ // replaces present array with new one
+//        content = e;
+//    }
+//
+//    @Override
+//    public String toString(){
+//        return Arrays.toString(content);
+//    }
+//}
